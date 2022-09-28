8da88d14f2e17bdf25cb01478f4155a8191d428c
davidmgudeman <davidmgudeman@gmail.com>
2022-09-28 14:08:44 -0700

    added quote list

--- src/pages/Quotes.js
+++ src/pages/Quotes.js
@@ -1,15 +1,20 @@
 
 
+
 import React, {Fragment} from 'react';
+import QuoteList from '../components/quotes/QuoteList';
+
+const DUMMY_QUOTES = [
+    {id: 'q1', author: 'Dave', text:"learning react is fun"},
+    {id: 'q2', author: 'Joe', text:"learning react is kinda fun"},
+    {id: 'q3', author: 'Momma', text:"learning react is very fun"},
+];
 
 const Quotes = () => {
     return (
         <Fragment>
         <main>
-        <ul>
-        <li>quote 1</li>
-        <li>quote 2</li>
-        </ul>
+       <QuoteList quotes={DUMMY_QUOTES}/>
         </main>
         </Fragment>
     )
